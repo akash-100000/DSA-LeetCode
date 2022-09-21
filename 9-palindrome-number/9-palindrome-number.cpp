c74b6ce8ae2121dcebddaa72e9f9808a71797d84
Akash Kumar <93013808+akash-100000@users.noreply.github.com>
2022-09-21 13:55:55 +0530

    Time: 23 ms (55.40%), Space: 5.9 MB (31.00%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -2,11 +2,12 @@ class Solution {
 public:
     bool isPalindrome(int x) {
         long reverse = 0;
-         if(x<0){
-            return 0;
-        }
+        
         long temp = x;
          while(x){
+              if(x<0){
+            return 0;
+        }
            reverse = reverse*10+x%10;
             x = x/10;
         }
